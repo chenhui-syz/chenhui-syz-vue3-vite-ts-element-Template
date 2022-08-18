fc98e8588907ff4abdff1b804c6c74d62d39f5f4
chenhui <8794986+gzchenhui@user.noreply.gitee.com>
2022-08-18 23:07:10 +0800

    feat999

--- command/build.js
+++ command/build.js
@@ -2,29 +2,30 @@
 // 打包因为都是用的node，所以这里都是使用commonjs规范
 const path = require("path");
 const { defineConfig, build } = require("vite");
-const vue = require("@/vitejs/plugin-vue");
+const vue = require("@vitejs/plugin-vue");
 const vueJsx = require("@vitejs/plugin-vue-jsx");
-const { default: defaultConfig } = require("wangeditor/dist/config");
+const fsExtra = require("fs-extra");
+const fs = require("fs");
 
 // 打包入口文件夹
 // packages文件为打包入口文件夹，将components文件夹下的组件代码手动复制进这个文件夹中
 // hooks，style，utils三个文件夹都复制进package文件夹中
 // packages文件夹中关于hooks和utils文件的引用路径记得改一下，否则打包之后会报错
-const entryDir = path.resolve(__dirname, "../package");
+const entryDir = path.resolve(__dirname, "../packages");
 
 // 出口文件夹
 // 这个文件夹不需要手动建，一会打包的时候会自动生成
 const outDir = path.resolve(__dirname, "../lib");
 
 // vite基础配置
-const baseConfig = defaultConfig({
+const baseConfig = defineConfig({
   configFile: false,
   publicDir: false,
   plugins: [vue(), vueJsx()],
 });
 
 // rollup配置
-const rollOptions = {
+const rollupOptions = {
   external: ["vue", "vue-router"],
   output: {
     globals: {
@@ -52,9 +53,71 @@ const buildAll = async () => {
   });
 };
 
+// 单组件打包构建
+// name：组件名称
+const buildSingle = async (name) => {
+  await build({
+    ...baseConfig,
+    build: {
+      rollupOptions,
+      lib: {
+        entry: path.resolve(entryDir, name),
+        // 打包后的名字
+        name: "index",
+        fileName: "index",
+        // 输出内容格式
+        formats: ["es", "umd"],
+      },
+      outDir: path.resolve(outDir, name),
+    },
+  });
+};
+
+// 给每个组件单独生成package.json
+const createPackageJson = (name) => {
+  const fileStr = `
+    {
+      "name": "${name}",
+      "main": "index.umd.js",
+      "module": "index.es.js",
+      "style": "styles.css"
+    }
+  `;
+  // 输出
+  // 这里需要安装个插件
+  // npm i -D fs-extra
+  // 因为vite已经升级到3版本，所以要先把对应的插件也升级下，npm i @vitejs/plugin-vue@2.3.3
+  fsExtra.outputFile(
+    path.resolve(outDir, `${name}/package.json`),
+    fileStr,
+    "utf-8"
+  );
+};
+
 // 打包成库
 const buildLib = async () => {
   await buildAll();
+
+  // 获取组件名称组成的数组
+  console.log('运行到这里了吗1111')
+  const components = fs.readdirSync(entryDir).filter((name) => {
+    const componentDir = path.resolve(entryDir, name);
+    // 判断是不是一个目录
+    const isDir = fs.lstatSync(componentDir).isDirectory();
+    // 如果是目录，并且目录下有index.ts文件，则进行返回
+
+    return isDir && fs.readdirSync(componentDir).includes("index.ts");
+    // utils文件夹是不需要的，可以手动删除，或者在这里过滤掉
+  });
+
+  
+
+  // 循环构建
+  for (const name of components) {
+    await buildSingle(name);
+    createPackageJson(name);
+  }
+  // 要想能够成功循环构建，还需要改造下package.json里的lib脚本，在执行原有脚本的同时拷贝一份package.json
 };
 
 buildLib();
--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,25 @@ import "element-plus/dist/index.css";
 
 import { toLine } from "./utils/index";
 
-import './mock'
+import "./mock";
 
 // 全局引用自己封装的组件
-import rcUI from './components/index'
+// import rcUI from './components/index'
 // 如果是按需引入呢？那么在这里直接引用单个的组件就行了，同样也在下面use链式调用就可以了
 // 如：
 // import chooseIcon from './components/chooseIcon/index'
+// 组件库打包后则可以换成packages里面的代码就行
+import rcUI from "../lib/rc-element-plus-components.mjs";
+import "../lib/style.css";
+// 单独引入组件
+import chooseIcon from "../lib/chooseIcon/index.mjs";
+import "../lib/chooseIcon/style.css";
+import form from "../lib/form/index.mjs";
+import modalForm from "../lib/modalForm/index.mjs";
+import table from "../lib/table/index.mjs";
+import "../lib/table/style.css";
+import calendar from "../lib/calendar/index.mjs";
+import "../lib/calendar/style.css";
 
 // 全局引用图标组件
 import * as Icons from "@element-plus/icons";
@@ -27,6 +39,15 @@ for (let i in Icons) {
   app.component(`el-icon-${toLine(i)}`, (Icons as any)[i]);
 }
 // 链式调用
-app.use(router).use(ElementPlus).use(rcUI);
+// 单独的use每一个组件
+app
+  .use(router)
+  .use(ElementPlus)
+  .use(chooseIcon)
+  .use(calendar)
+  .use(form)
+  .use(modalForm)
+  .use(table);
+// .use(rcUI);
 
 app.mount("#app");
